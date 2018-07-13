1222ae31f6be5677d0705f3e8b69a84055322697
Rory Dahl <rsdah13@gmail.com>
2018-07-13 11:38:20 -0700

    add apache config for zip files. Change production settings to render from react instead of django template.

--- configuration/configuration/settings.py
+++ configuration/configuration/settings.py
@@ -1,15 +1,31 @@
 from .dev_settings import *
 
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-#
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "assets","bundles"),
-# ]
-#
-# WEBPACK_LOADER = {
-#     'DEFAULT': {
-#             'BUNDLE_DIR_NAME': 'bundles/',
-#             'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.prod.json'),
-#         }
-# }
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets","bundles"),
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, "assets", "bundles"), ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'bundles/',
+            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.prod.json'),
+        }
+}
